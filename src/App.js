4be3b1cfb89e0e78b7df9a61dead6b79843a5072
Sagar Teria <sagarteria@gmail.com>
2021-04-29 02:12:08 +0530

    Completed example of functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionalComponent from './components/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <FunctionalComponent />
     </div>
   );
 }
--- src/components/FunctionalComponent.js
+++ src/components/FunctionalComponent.js
@@ -1,1 +1,7 @@
 import React from 'react';
+
+function FunctionalComponent() {
+  return <h1>This is Functional Component</h1>
+}
+
+export default FunctionalComponent
