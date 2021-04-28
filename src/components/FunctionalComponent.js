3d6f548bfce17b1d44f25262e0696f8f0e97f355
Sagar Teria <sagarteria@gmail.com>
2021-04-29 02:18:45 +0530

    Functional Components with named export

--- src/components/FunctionalComponent.js
+++ src/components/FunctionalComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
-const FunctionalComponent = () => <h1>This is Functional Component (ES6 Syntax)</h1>
+// example of named export
+export const FunctionalComponent = () => <h1>This is Functional Component (ES6 Syntax)</h1>
 
-export default FunctionalComponent
+//export default FunctionalComponent
