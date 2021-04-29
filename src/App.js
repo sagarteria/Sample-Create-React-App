7753c353e205e4920bc3937661342a1faaba36a7
Sagar Teria <sagarteria@gmail.com>
2021-04-30 01:26:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { FunctionalComponent } from './components/FunctionalComponent';
 import ClassComponent from './components/ClassComponent';
+import FunctionalComponentJSXExample from './components/FunctionalComponentJSXExample';
 
 function App() {
   return (
     <div className="App">
       <FunctionalComponent />
       <ClassComponent />
+      <FunctionalComponentJSXExample />
     </div>
   );
 }
