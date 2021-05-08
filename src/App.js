683e9f42a6d164e28491eb3b9784e22c1c8c7c77
Sagar Teria <sagarteria@gmail.com>
2021-05-08 20:18:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import StateClassExample from './components/StateClassExample';
 import Counter from './components/Counter';
 import FunctionalCompDestructure from './components/DestructuringPropsStates/FunctionalCompDestructure';
 import ClassCompDestructure from './components/DestructuringPropsStates/ClassCompDestructure';
+import FunctionClick from './components/Events/FunctionClick';
 
 
 function App() {
@@ -32,9 +33,10 @@ function App() {
       <Counter />
 
       <FunctionalCompDestructure FirstProp='Name1' SecondProp='Name2'/>
-
       <ClassCompDestructure FirstProp='Name1' SecondProp='Name2'/>
 
+      <FunctionClick />
+      
     </div>
   );
 }
