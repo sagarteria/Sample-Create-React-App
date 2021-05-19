d396585ccd471af65e1e557a4be1d6ad2299ee27
Sagar Teria <sagarteria@gmail.com>
2021-05-19 22:35:24 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { FunctionalComponent } from './components/FunctionalComponent';
-import ClassComponent from './components/ClassComponent';
-import FunctionalComponentJSXExample from './components/FunctionalComponentJSXExample';
-import FunctionalComponentJSExample from './components/FunctionalComponentJSExample';
-import PropsFunctionalExample from './components/PropsFunctionalExample';
-import StateClassExample from './components/StateClassExample';
-import Counter from './components/Counter';
-import FunctionalCompDestructure from './components/DestructuringPropsStates/FunctionalCompDestructure';
-import ClassCompDestructure from './components/DestructuringPropsStates/ClassCompDestructure';
-import FunctionClick from './components/Events/FunctionClick';
-import ClassClick from './components/Events/ClassClick';
-import EventBind from './components/Events/EventBinding/EventBind';
-import OuterComponent from './components/ChildParentCommunication/OuterComponent';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { FunctionalComponent } from "./components/FunctionalComponent";
+import ClassComponent from "./components/ClassComponent";
+import FunctionalComponentJSXExample from "./components/FunctionalComponentJSXExample";
+import FunctionalComponentJSExample from "./components/FunctionalComponentJSExample";
+import PropsFunctionalExample from "./components/PropsFunctionalExample";
+import StateClassExample from "./components/StateClassExample";
+import Counter from "./components/Counter";
+import FunctionalCompDestructure from "./components/DestructuringPropsStates/FunctionalCompDestructure";
+import ClassCompDestructure from "./components/DestructuringPropsStates/ClassCompDestructure";
+import FunctionClick from "./components/Events/FunctionClick";
+import ClassClick from "./components/Events/ClassClick";
+import EventBind from "./components/Events/EventBinding/EventBind";
+import OuterComponent from "./components/ChildParentCommunication/OuterComponent";
+import ClassCompIfElse from "./components/ConditionalRendering/ClassCompIfElse";
 
 function App() {
   return (
@@ -23,29 +23,32 @@ function App() {
       <FunctionalComponentJSXExample />
       <FunctionalComponentJSExample />
 
-      <PropsFunctionalExample name='Name1'>
+      <PropsFunctionalExample name="Name1">
         <p>This is children props</p>
       </PropsFunctionalExample>
-      <PropsFunctionalExample name='Name2'>
+      <PropsFunctionalExample name="Name2">
         <button>Action</button>
       </PropsFunctionalExample>
-      <PropsFunctionalExample name='Name3'></PropsFunctionalExample>
+      <PropsFunctionalExample name="Name3"></PropsFunctionalExample>
 
       <StateClassExample />
 
       <Counter />
 
-      <FunctionalCompDestructure FirstProp='Name1' SecondProp='Name2'/>
-      <ClassCompDestructure FirstProp='Name1' SecondProp='Name2'/>
+      <FunctionalCompDestructure FirstProp="Name1" SecondProp="Name2" />
+      <ClassCompDestructure FirstProp="Name1" SecondProp="Name2" />
 
       {/* Event Bind Examples */}
       <FunctionClick />
       <ClassClick />
       <EventBind />
 
-      {/* outer inner component interaction */}
+      {/* Outer inner component interaction */}
       <OuterComponent />
-      
+
+      {/* Conditional Rendering Examples */}
+      {/* 1. If-Else */}
+      <ClassCompIfElse />
     </div>
   );
 }
