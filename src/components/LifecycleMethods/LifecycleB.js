34167120c00d245ec1c624c0eac64b3b99763cd4
Sagar Teria <sagarteria@gmail.com>
2021-05-24 15:19:40 +0000

    initial commit

---
+++ src/components/LifecycleMethods/LifecycleB.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class LifecycleA extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'name1'
+    }
+
+    console.log('LifecycleA constructor');
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('LifecycleA getDerivedStateFromProps');
+    return null
+  }
+
+  componentDidMount(){
+    console.log('LifecycleA componentDidMount');
+  }
+
+  render () {
+    console.log('LifecycleA return');
+    return (
+      <>
+      <hr />
+      <h2>LifeCycle Methods</h2>
+      <h3>LifeCycle A</h3>
+      </>
+    )
+  }
+}
+
+export default LifecycleA
